82422aee4db6401f0290a116e31614150ac8b64d
deanqkhanhcoder <pquockhanh09102010@gmail.com>
2025-04-02 16:07:32 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/music-player/' : '/',
+  base: mode === 'production' ? '/music-project/' : '/',
   server: {
     host: "::",
     port: 8080,
